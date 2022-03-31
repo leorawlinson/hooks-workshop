73ccb6451e77f36332c627b6288377c5cd86a573
Leo Rawlinson <leo.m.rawlinson@gmail.com>
2022-03-31 17:34:45 +0200

    Exercise one complete

--- lessons/01-rendering/exercise/SignupForm.js
+++ lessons/01-rendering/exercise/SignupForm.js
@@ -8,5 +8,20 @@ import { DateFields, MonthField, DayField, YearField } from "app/DateFields"
 // export default SignupForm
 
 export default function SignupForm() {
-  return <form className="SignupForm">{/* Your work here */}</form>
+  return (
+    <form className="SignupForm">
+      <div>
+        <input type="text" placeholder="Display Name" />
+        <input type="text" placeholder="Photo URL" />
+        <input type="text" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+      </div>
+      <DateFields defaultValue={new Date()}>
+        <MonthField />
+        <DayField />
+        <YearField start={1900} end={2100} />
+      </DateFields>
+      <TabsButton>Submit</TabsButton>
+    </form>
+  )
 }
--- modules/app/TabsButton.js
+++ modules/app/TabsButton.js
@@ -1,8 +1,10 @@
 import React from "react"
+import { FaDumbbell } from "react-icons/fa"
 
 export default function TabsButton({ children }) {
   return (
     <button className="TabsButton icon_button cta" type="submit">
+      <FaDumbbell />
       {children}
     </button>
   )
